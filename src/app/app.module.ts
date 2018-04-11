b9d38b60a49ec9913e157a156f6b31299a793c2c
Fernanda Lopez <FernandaLopez@Fernandas-MacBook-Pro.local>
2018-04-11 10:17:25 -0500

    stepper

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { StepperComponent } from './stepper/stepper.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StepperComponent
   ],
   imports: [
     BrowserModule
